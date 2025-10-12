efd1072b9dd181fa1edb0baeb3c30e779a88d03e
panda-piglet <18875812593@163.com>
2025-10-12 22:21:18 +0800

    Add files via upload

--- AS1/AS1_Q6.py
+++ AS1/AS1_Q6.py
@@ -22,11 +22,40 @@ def main():
         for i in range(1,maxrange+1,1):
             sum=0
             #二层循环找i的所有因数
-            for j in range(1,i,1):
+            for j in range(1,i//2+1,1):
                 if i%j==0:
                     sum += j
             if sum == i:
                 print(sum)
 
+def is_perfect(num):
+    sum=0
+    for i in range(1,num//2+1,1):
+        if num%i == 0:
+            sum += i
+    if sum == num:
+        return True
+    return False
+
+
+#利用公式迅速找完全数
+def fast():
+    maxrange=input()
+    if "."in maxrange or int(maxrange) <= 0:
+        print("Illegal input!")
+    else:
+        maxrange=int(maxrange)
+        print(f"The perfect numbers smaller than {maxrange} are:")
+        p=2
+        while True:
+            possible_perfect_number=2**(p-1)*(2**p-1)
+            if possible_perfect_number > maxrange:
+                break
+            if is_perfect(possible_perfect_number):
+                print(possible_perfect_number)
+            p +=1
+def test(p):
+    possible_perfect_number=2**(p-1)*(2**p-1)
+    print(possible_perfect_number)
 if __name__=="__main__":
-    main()
+    cheat()
