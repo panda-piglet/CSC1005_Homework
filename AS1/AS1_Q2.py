e0e4e14c96956a8143a6b83c5ae290856a9ccf4f
panda-piglet <18875812593@163.com>
2025-10-16 17:43:38 +0800

     代码重构，提升效率

--- AS1/AS1_Q2.py
+++ AS1/AS1_Q2.py
@@ -1,40 +1,22 @@
-#埃氏筛，
-"""def eratosthenes_sieve(n):
-    if n < 2:
-        return []
-
-    is_prime = [True]*n
-    #Zero and one are not prime
-    is_prime[0] = is_prime[1] = False
-    for i in range(2 , int(n**5)+1 , 1):
-    #还没有被筛掉，是质数
-        if is_prime[i] == True:
-            #标记他的倍数为合数
-            for j in range(i**2 , n , i):
-                is_prime[j]= False
-    return is_prime
-
-def liner_sieve(n):
-    pass
-"""
-import math
-def is_prime(num):
-    if num==1:
-        return False
-    if num==2:
-        return True
-    element=int(math.sqrt(num))+1
-    for i in range(2,element+1,1):
-        if num%i == 0:
+#判断质数
+def is_prime(x):
+    if x < 2: return False
+    if x % 2 == 0:
+        return x == 2
+    division = 3 
+    root = int(x**0.5)
+    while division <= root:
+        if x % division == 0: 
             return False
+        division += 2
     return True
+
 def main():
     num=int(input())
     if is_prime(num):
         print(f"{num} is a prime number.")
     else:
         print(f"{num} is not a prime number.")
-
 
 if __name__ == "__main__":
     main()
