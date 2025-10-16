15f9d30cd5c8f761330292bd12381dc84d45e865
panda-piglet <18875812593@163.com>
2025-10-16 17:36:01 +0800

    使用分析函数让代码更加简洁

--- AS1/AS1_Q4.py
+++ AS1/AS1_Q4.py
@@ -1,20 +1,23 @@
 import math
+def parse_positive_int(s):
+    s=s.strip()
+    if s.startswith('0'):
+        raise ValueError
+    elif not s.isdigit():
+        raise ValueError
+    n=int(s)
+    return n
+
 def main():
     num=input()
-    if "." in num:
+    try:
+        num=parse_positive_int(num)
+    except ValueError:
         print("Illegal input!")
     else:
-        num = int(num)
-        if num < 0:
-            print("Illegal input!")
-        else:
-            factorial = math.factorial(num)
-            """if num == 0:
-                pass
-            else:
-                for i in range(1,num+1,1):
-                    factorial *= i"""
-            print (f"The factorial of {num} is: {factorial}")
+        
+        factorial = math.factorial(num)
+        print (f"The factorial of {num} is: {factorial}")
 
 
 if __name__ == "__main__":
--- AS1/AS1_Q5.py
+++ AS1/AS1_Q5.py
@@ -1,5 +1,7 @@
 def main():
+    #将输入的数字存入列表
     nums=map(int,input().split())
     print(f"The largest number is: {max(nums)}")
+
 if __name__=="__main__":
     main()
