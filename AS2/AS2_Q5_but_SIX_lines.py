bdda700ed047ef77fc80a56281848542f41a957f
panda-piglet <18875812593@163.com>
2025-11-12 15:58:37 +0800

    二重排序，代码已经可以通关
    Sort the lists of characters by frequency in the dictionary.

--- AS2/AS2_Q5_but_SIX_lines.py
+++ AS2/AS2_Q5_but_SIX_lines.py
@@ -3,6 +3,8 @@ def character_frequency(characters):
     for element in characters:
         if element not in dic.get(characters.count(element), []):
             dic[characters.count(element)] = dic.get(characters.count(element), []) + [element]
+    for key_name in dic:
+        dic[key_name] = sorted (dic[key_name])
     return dict(sorted(dic.items()))
 
 def test():
