4c2c9a7a6f51edb997b27992a22d5d25ef644f2e
panda-piglet <18875812593@163.com>
2025-11-24 16:33:31 +0800

    the matrix class will return a new matrix object

--- AS3/AS3_Q1.py
+++ AS3/AS3_Q1.py
@@ -1,26 +1,39 @@
-class Matric:
-    def __init__(self,a0,a1,b0,b1):
-        self.matric=[[a0,a1],[b0,b1]]
+def creat_matrix(*args):
+    args = list(map(float,args))
+    dimention = len(args)**0.5
+    if int(dimention) != dimention:
+        raise ValueError
+    dimention = int(dimention)
+    matrix = [[args[dimention*j + i] for i in range(dimention)]for j in range(dimention)]
+    return matrix
+
+class DoubleDimentionMatrix:
+    def __init__(self,matrix):
+        self.dimention = len(matrix)
+        self.matrix = matrix
+    def __repr__(self):
+        return str(self.matrix)
     def __add__(self,another):
-        return [[self.matric[0][0] + another.matric[0][0],self.matric[0][1] +another.matric[0][1]],
-                [self.matric[1][0] + another.matric[1][0],self.matric[1][1] + another.matric[1][1]]]
+        return DoubleDimentionMatrix([[self.matrix[0][0] + another.matrix[0][0],self.matrix[0][1] +another.matrix[0][1]],
+                [self.matrix[1][0] + another.matrix[1][0],self.matrix[1][1] + another.matrix[1][1]]])
     def __sub__(self,another):
-        return [[self.matric[0][0] - another.matric[0][0],self.matric[0][1] - another.matric[0][1]],
-                [self.matric[1][0] - another.matric[1][0],self.matric[1][1] - another.matric[1][1]]]
+        return DoubleDimentionMatrix([[self.matrix[0][0] - another.matrix[0][0],self.matrix[0][1] - another.matrix[0][1]],
+                [self.matrix[1][0] - another.matrix[1][0],self.matrix[1][1] - another.matrix[1][1]]])
     def __mul__(self,another):
-        return [[self.matric[0][0] * another.matric[0][0] + self.matric[0][1] * another.matric[1][0],
-                self.matric[0][0] * another.matric[0][1] + self.matric[0][1] * another.matric[1][1]],
-                [self.matric[1][0] * another.matric[0][0] + self.matric[1][1] * another.matric[1][0],
-                self.matric[1][0] * another.matric[0][1] + self.matric[1][1] * another.matric[1][1]]]
+        return DoubleDimentionMatrix([[self.matrix[0][0] * another.matrix[0][0] + self.matrix[0][1] * another.matrix[1][0],
+                self.matrix[0][0] * another.matrix[0][1] + self.matrix[0][1] * another.matrix[1][1]],
+                [self.matrix[1][0] * another.matrix[0][0] + self.matrix[1][1] * another.matrix[1][0],
+                self.matrix[1][0] * another.matrix[0][1] + self.matrix[1][1] * another.matrix[1][1]]])
     def calculate(self):
-        return self.matric[0][0]*self.matric[1][1] - self.matric[0][1] *self.matric[1][0]
+        return self.matrix[0][0]*self.matrix[1][1] - self.matrix[0][1] *self.matrix[1][0]
 
 def test():
-    matrix1 = Matric(1,2,3,4)
-    matrix2 = Matric(5,6,7,8)
+    matrix1 = DoubleDimentionMatrix(creat_matrix(1,2,3,4))
+    matrix2 = DoubleDimentionMatrix(creat_matrix(5,6,7,8))
     #print(matrix1 + matrix2)
     print(f"matrix1 + matrix2 resulting in {matrix1 + matrix2}")
     print(f"matrix1 - matrix2 resulting in {matrix1 - matrix2}")
     print(f"matrix1 * matrix2 resulting in {matrix1 * matrix2}")
+    #print(creat_matrix(1,2,3,4))
 if __name__ == "__main__":
     test()
