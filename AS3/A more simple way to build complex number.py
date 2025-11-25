8fc718c0ad3f07fcc8d819c0f08dad054c16a8b3
panda-piglet <18875812593@163.com>
2025-11-25 16:45:55 +0800

    somethiong more for Q2

---
+++ AS3/A more simple way to build complex number.py
@@ -0,0 +1,21 @@
+def p_complex(complexnumber):
+        result = []
+        tmp = 0
+        for index in range(0,len(complexnumber)):
+            if complexnumber [index] == "+" or complexnumber [index] == "-":
+                result.append(complexnumber[tmp:index])
+                tmp = index
+        result.append(complexnumber[tmp::])
+        real = 0
+        imag = 0
+        try:
+            for element in result:
+                if element.endswith("*i"):
+                    imag += float(element[:-2:])
+                else:
+                    real += float(element)
+        except ValueError:
+            print("This is not a complex number")
+        return real,imag
+    
+print(p_complex("3.1+2.2*i"))
